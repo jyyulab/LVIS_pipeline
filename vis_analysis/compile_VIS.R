ae479f27a46a08cead7f481a81cd63d42c805ae5
Yan <koonkiu.yan@gmail.com>
2021-07-12 22:36:59 -0500

    clean up pipelines

--- vis_analysis/compile_VIS.R
+++ vis_analysis/compile_VIS.R
@@ -2,27 +2,21 @@
 ##Outputs of different samples are concatenated vertically, and an extra column called sample_ori is added. 
 ##The format of the column sample.ori is
 ##patient_celltype_time, e.g. P1_CD3_15mo
-##Ususally, patients are analyzed separately.
+##Bedtools and the R package bedr are required.
 
-load("~/Desktop/LVXSCID_manuscript/parsed_VIS/all_P1_collected_IS_rename.RData");
-dim(all_P1_collected_IS_rename)
-setwd("~/Github/LVIS_pipeline/vis_analysis/")
+load("all_P4_collected_IS.RData");
+source("LVIS_functions.R");
+load("hg19.basic.annotation.update.Rdata");
 
-source("~/Github/LVIS_pipeline/vis_analysis/LVIS_functions.R");
-#load("hg19.basic.annotation.update.Rdata");
+###In some of the early experiments, spikeIn were used. 
+#but the column is not meaingful here.
 
-###In some of the early experiments, spikeIn were used. We remove all sites overlap with the spike.
+all_expts<-unique(all_P4_collected_IS$sample_ori);
 
-iz1<-which(all_P1_collected_IS_rename$nOverlapWithSpike==0)
-all_P1_collected_IS_rename<-all_P1_collected_IS_rename[iz1,];
-
-all_expts<-unique(all_P1_collected_IS_rename$sample_ori);
-
-###To analyze the sites in different samples of the same patient , we need to match sites across samples. 
+###To analyze the sites in different samples of the same patient, we need to match sites across samples. 
 ###In short, two sites from two samples which overlap with each other are identifed as the same site (strand specific).
-###Bedtools and the R package bedr are required.
 
-res<-get_merged_ID_from_all_VIS_collection(all_P1_collected_IS_rename);
+res<-get_merged_ID_from_all_VIS_collection(all_P4_collected_IS);
 
 u_VIS_merge<-res[['u_VIS_merge']];
 dim(u_VIS_merge)
@@ -36,7 +30,7 @@ X<-res[['X']];
 ###The list of insertion sites are re-annotated using HOMER. First, an input file is prepared.
 u_VIS_merge_out<-cbind(PeakID=rownames(u_VIS_merge),u_VIS_merge[,1:3],Strand=u_VIS_merge[,6]);
 colnames(u_VIS_merge_out)<-c("PeakID","Chr","Start","End","Strand");
-write.table(u_VIS_merge_out,file="all_P1_VIS_merge.txt",sep="\t",quote=FALSE,row.names = FALSE);
+write.table(u_VIS_merge_out,file="all_P4_VIS_merge.txt",sep="\t",quote=FALSE,row.names = FALSE);
 
 ###Then HOMER is run separately. The annotation is stored in the out.txt file.
 VIS_list="all_P1_VIS_merge";
@@ -44,25 +38,39 @@ module load homer/4.9.1
 annotatePeaks.pl $VIS_list.txt hg19 -annStats $VIS_list.homer.stat.txt  > $VIS_list.homer.out.txt
 
 library(openxlsx);
-tmp<-read.xlsx('all_P1_VIS_merge.homer.out.xlsx');
+tmp<-read.xlsx('all_P4_VIS_merge.homer.out.xlsx');
 rownames(tmp)<-tmp[,1];
 u_VIS_merge_homer_annot<-tmp[rownames(u_VIS_merge),];
 
-####Finally, we build two count matrices, one for the number of reads mapped to the sites in all samples
-####one for the number of unique shear sites reads mapped to the sites in all samples
+####Finally, we build two count matrices, one for the number of reads mapped to each of the sites in all samples
+####one for the number of unique shear sites reads mapped to the sites in all samples.
+###The total number of reads mapped to a VIS was used to quantify the relative population of the corresponding clone. 
+
 iu<-which(!is.na(X$u_merge_ind))
 X<-X[iu,];
 counts_nR_IS_expts<-generate_u_VIS_merge_samples_matrix(X);
-dim(counts_nR_IS_expts)#10270    58
+dim(counts_nR_IS_expts)
 
 counts_nSS_IS_expts<-generate_u_VIS_merge_samples_matrix_unique(X);
 dim(counts_nSS_IS_expts)#
 
-all_P1_collected_IS_clean<-all_P1_collected_IS_rename;
+all_P4_collected_IS_clean<-all_P4_collected_IS;
 
-save(all_P1_collected_IS_clean,counts_nR_IS_expts,counts_nSS_IS_expts,u_VIS_merge_homer_annot,u_VIS_merge,file="all_P1_source.Rdata");
+save(all_P4_collected_IS_clean,counts_nR_IS_expts,counts_nSS_IS_expts,u_VIS_merge_homer_annot,u_VIS_merge,file="all_P4_source.Rdata");
 
+#the default thresholds of integration sites built in the qsLAM pipeline are with at least two unique reads or at least five total reads were kept. 
+#To reduce false-positive results, 1.5% of reads are removed
 
+low_abundance_cutoff<-0.015;
+counts_nR_IS_expts_Filter<-filter_vis(counts_nR_IS_expts,low_abundance_cutoff);
+
+iz<-which(rowSums(counts_nR_IS_expts_Filter)>0);
+counts_nR_IS_expts_Filter<-counts_nR_IS_expts_Filter[iz,];
+u_VIS_merge_homer_annot_Filter<-u_VIS_merge_homer_annot[iz,];
+u_VIS_merge_Filter<-u_VIS_merge[iz,];
+
+out_file<-"P4_filter_source.Rdata");
+save(u_VIS_merge_homer_annot_Filter,u_VIS_merge_Filter,counts_nR_IS_expts_Filter,file=out_file);
 
 
 
--- vis_analysis/vis_visualization_examples.R
+++ vis_analysis/vis_visualization_examples.R
@@ -1,17 +1,11 @@
-##This pipeline starts from the outputs of the VIS calling pipeline.
-##Outputs of different samples are concatenated vertically, and an extra column called sample_ori is added. 
-##The format of the column sample.ori is
-##patient_celltype_time, e.g. P1_CD3_15mo
-##Ususally, patients are analyzed separately.
-
 source("LVIS_functions.R");
-#load("hg19.basic.annotation.update.Rdata");
+load("hg19.basic.annotation.update.Rdata");
 
-load("/Volumes/yu3grp/IO_JY/yu3grp/LVXSCID/VIS_20190708/parsed_VIS/all_P1_collected_IS_rename.RData");
-dim(all_P1_collected_IS_rename)
+load("all_P4_collected_IS.RData");
+dim(all_P4_collected_IS)
 
 ###To make pie charts of clone diversity. 
-res<-get_top20_composition_and_nVIS_all_samples(all_P1_collected_IS_rename);
+res<-get_top20_composition_and_nVIS_all_samples(all_P4_collected_IS);
 num_VIS<-res[['num_VIS']];
 top20_IS_freq_vs_expts<-res[['top20_IS_freq']];
 
@@ -20,8 +14,8 @@ df<-out$df;
 dt<-out$dt;
 
 ##focus on sites from P1 only
-df1<-df[which(df$expt.patients=='P1'),];
-dt1<-dt[which(dt$expt.patients=='P1'),];
+df1<-df[which(df$expt.patients=='P4'),];
+dt1<-dt[which(dt$expt.patients=='P4'),];
 
 df1$expt.Celltype<-factor(df1$expt.Celltype,levels=c('CD3','CD19','CD56','CD14CD15','TNC'));
 df1$expt.time<-factor(df1$expt.time,levels=c('12wks','16wks','6mo','9mo','12mo','15mo','16mo','18mo','21mo','24mo','27mo','30mo'));
@@ -30,10 +24,10 @@ dt1$expt.Celltype<-factor(dt$expt.Celltype,levels=c('CD3','CD19','CD56','CD14CD1
 dt1$expt.time<-factor(dt$expt.time,levels=c('12wks','16wks','6mo','9mo','12mo','18mo','24mo'));
 
 p1<-draw_pie_charts(df1,dt1);p1;
-ggsave("all_P1_samples_VIS_composition.pdf",plot = last_plot(), device = NULL, path = NULL,scale = 1, width = 10, height = 14, dpi = 300);
+ggsave("all_P4_samples_VIS_composition.pdf",plot = last_plot(), device = NULL, path = NULL,scale = 1, width = 10, height = 14, dpi = 300);
 
 ###We implemented a few metrics to quantify sample diveristy, including entropy, Chao estimator, OCI, UC50.
-samples_diversity<-get_diversity_measures_all_samples(all_P1_collected_IS_rename);
+samples_diversity<-get_diversity_measures_all_samples(all_P4_collected_IS);
 
 
 ####To generate GenesCloud
@@ -41,6 +35,6 @@ load(paste0(fcts_loc,"hg19.basic.annotation.update.Rdata"));
 u_VIS_merge_homer_annot_modify<-simplify_homer_annotation(u_VIS_merge_homer_annot,M);
 
 geneFreq<-get_geneFreq(u_VIS_merge_homer_annot_modify);
-generate_GeneCloud(geneFreq,'P2_geneCloud.pdf');
+generate_GeneCloud(geneFreq,'P4_geneCloud.pdf');
 
 
